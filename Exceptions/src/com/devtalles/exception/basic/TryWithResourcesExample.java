ac75070d99b472ca5e71b885462edf323e1b3b10
JorgeLuisSoria <jorge.luis.soria@outlook.com>
2026-08-16 22:16:02 -0500

    example of try with resources in java

--- Exceptions/src/com/devtalles/exception/basic/TryWithResourcesExample.java
+++ Exceptions/src/com/devtalles/exception/basic/TryWithResourcesExample.java
@@ -1,4 +1,21 @@
 package com.devtalles.exception.basic;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TryWithResourcesExample {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/example.txt"))) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
 }
