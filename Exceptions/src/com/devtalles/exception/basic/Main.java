4948e90e0f904d0566eb040f8bfee7160199999d
JorgeLuisSoria <jorge.luis.soria@outlook.com>
2026-08-16 23:47:03 -0500

    adding debugging in exceptions

--- Exceptions/src/com/devtalles/exception/basic/Main.java
+++ Exceptions/src/com/devtalles/exception/basic/Main.java
@@ -14,8 +14,22 @@ public class Main {
             System.out.println("The result of the operation is: " + result);
         } catch (ArithmeticException e) {
             System.out.println("The operation just failed: " + e.getMessage());
+            e.printStackTrace();
         } finally {
             System.out.println("The operation has been executed.");
+        }
+
+        try {
+            int[] numbers = new int[3];
+            System.out.println(numbers[6]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            StackTraceElement[] stackTraceElements = e.getStackTrace();
+            for (StackTraceElement stackTraceElement : stackTraceElements) {
+                System.out.println("Class: " + stackTraceElement.getClassName());
+                System.out.println("Method: " + stackTraceElement.getMethodName());
+                System.out.println("Line: " + stackTraceElement.getLineNumber());
+                System.out.println("File: " + stackTraceElement.getFileName());
+            }
         }
     }
 }
