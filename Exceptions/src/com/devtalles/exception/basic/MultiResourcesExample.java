917ef693e73fff4ac8299652f078e09c6f98c3b2
JorgeLuisSoria <jorge.luis.soria@outlook.com>
2026-08-16 22:49:28 -0500

    try with Resources with Multiple rsrcs

---
+++ Exceptions/src/com/devtalles/exception/basic/MultiResourcesExample.java
@@ -0,0 +1,25 @@
+package com.devtalles.exception.basic;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MultiResourcesExample {
+    public static void main(String[] args) {
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader("src/example.txt"));
+                BufferedReader reader2 = new BufferedReader(new FileReader("src/example2.txt"));
+        ) {
+            String line;
+            while((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println("Reader 2: " + reader2.readLine());
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
